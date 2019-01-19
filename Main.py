390904c913cc74a1afb8f545d84ab1e2cbfb7a6a
Stasivs <stasivs@bk.ru>
2019-01-19 22:08:52 +0300

    Added screen and game loop

--- Main.py
+++ Main.py
@@ -1,6 +1,17 @@
 import pygame
-
+from Location import location
 
 pygame.init()
-size = width, height = 800, 600
+size = width, height = (1216, 1216)
+screen = pygame.display.set_mode(size)
+running = True
 
+loc = location(screen)
+loc.render()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+pygame.quit()
