6d3ee535b107c62158e38f66e96684fe9c7fbb4b
Stasivs <stasivs@bk.ru>
2019-01-19 22:06:30 +0300

    Revert "Added screen and game loop"
    This reverts commit d72253a124e91997111aed3efd34f0fce91c2b57.

--- Main.py
+++ Main.py
@@ -1,17 +1,6 @@
 import pygame
-from Location import location
+
 
 pygame.init()
-size = width, height = (1216, 1216)
-screen = pygame.display.set_mode(size)
-running = True
+size = width, height = 800, 600
 
-loc = location(screen)
-loc.render()
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    pygame.display.flip()
-pygame.quit()
