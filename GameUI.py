6e419f12d5e24d94ebe4e552984f3659ac888942
Stasivs <stasivs@bk.ru>
2019-02-15 01:27:32 +0300

    Add to Button class on_click method and possibility to add some buttons to the MessageBox

--- GameUI.py
+++ GameUI.py
@@ -23,8 +23,8 @@ class BaseForm (pygame.sprite.Group):
 
 
 class BaseElement (pygame.sprite.Sprite):
-    def __init__(self, x, y, w, h):
-        super(BaseElement, self).__init__()
+    def __init__(self, x, y, w, h, group):
+        super(BaseElement, self).__init__(group)
         self.w, self.h = w, h
         self.x, self.y = x, y
 
@@ -39,19 +39,31 @@ class MessageBox (BaseForm):
     def __init__(self, message, *args, **kwargs):
         super(MessageBox, self).__init__(*args, **kwargs)
         self.message = message
+        self.text = "Кнопка"
+        self.buttons = []
+
+    def append(self, button):
+        self.buttons.append(button)
 
     def draw(self, surface):
         super(MessageBox, self).draw(surface)
         surface.blit(self.message, (self.x, self.y))
+        for button in self.buttons:
+            surface.blit(button.image, (self.x + button.rect.x, self.y  + button.rect.y))
 
 
 class Button (BaseElement):
     def __init__(self, button_text, *args, **kwargs):
         super(Button, self).__init__(*args, **kwargs)
         self.image.blit(button_text, (self.w // 2, self.h // 2))
+        self.rect.x = self.x
+        self.rect.y = self.y
 
-    def on_click(self):
-        pass
+    def on_click(self, event):
+        print(self.rect.collidepoint(event.pos))
+        print(event.pos, self.rect)
+        if self.rect.collidepoint(event.pos):
+            print("Ну нажал ты на меня, и что?")
 
 
 class MapButton (Button):
@@ -79,17 +91,20 @@ class Menu (BaseForm):
 
 
 if __name__ == "__main__":  # для тестирования классов интерфейса
-    mb = MessageBox(text("MessageBox 1", pygame.Color("Red")), 100, 100, 200, 50)
-    mb.add(Button(text("Button1", pygame.Color("Red")), 40, 40, 100, 10))
-
+    mb = MessageBox(text("MessageBox 1", pygame.Color("Red")), 100, 100, 200, 200)
+    mb.append(Button(text("Button1", pygame.Color("Red")), 0, 50, 200, 50, mb))
+    print(mb.buttons)
     running = True
     while running:
         for ev in pygame.event.get():
             mb.update(ev)
             if ev.type == pygame.QUIT:
                 running = False
+            if ev.type == pygame.MOUSEBUTTONDOWN:
+                for button in mb.buttons:
+                    button.on_click(ev)
 
-        screen.fill((0, 0, 0))
+        screen.fill((100, 100, 100))
         mb.draw(screen)
         pygame.display.flip()
 
