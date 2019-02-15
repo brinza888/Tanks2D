120d299069a19e0938b5ba1b0df8bc2f15661697
Brinza Bezrukoff <bezrukoff888@gmail.com>
2019-02-15 16:37:08 +0300

    GameUI changes

--- GameUI.py
+++ GameUI.py
@@ -10,12 +10,7 @@ class BaseForm (pygame.sprite.Group):
 
     def draw(self, surface):
         pygame.draw.rect(surface, self.bg_color, (self.x, self.y, self.w, self.h))
-
-    def add(self, *sprites):
-        super(BaseForm, self).add(*sprites)
-        for element in sprites:
-            element.rect.x = self.x + element.x
-            element.rect.y = self.y + element.y
+        super(BaseForm, self).draw(surface)
 
     def get_event(self, event):
         for element in self.sprites():
@@ -23,7 +18,7 @@ class BaseForm (pygame.sprite.Group):
 
 
 class BaseElement (pygame.sprite.Sprite):
-    def __init__(self, x, y, w, h, group):
+    def __init__(self, x, y, w, h, color, group):
         super(BaseElement, self).__init__(group)
         self.w, self.h = w, h
         self.x, self.y = x, y
@@ -39,18 +34,11 @@ class MessageBox (BaseForm):
     def __init__(self, message, *args, **kwargs):
         super(MessageBox, self).__init__(*args, **kwargs)
         self.message = message
-        self.text = "Кнопка"
-        self.buttons = []
-
-    def append(self, button):
-        button.rect.x += self.x
-        button.rect.y += self.y
-        self.buttons.append(button)
 
     def draw(self, surface):
         super(MessageBox, self).draw(surface)
         surface.blit(self.message, (self.x, self.y))
-        for button in self.buttons:
+        for button in self.sprites():
             surface.blit(button.image, (button.rect.x, button.rect.y))
 
 
@@ -61,11 +49,9 @@ class Button (BaseElement):
         self.rect.x = self.x
         self.rect.y = self.y
 
-    def on_click(self, event):
-        print(self.rect.collidepoint(event.pos))
-        print(event.pos, self.rect)
-        if self.rect.collidepoint(event.pos):
-            print("Ну нажал ты на меня, и что?")
+    def on_click(self):
+        print("Ну нажал ты на меня, и что?")  # действие по клику на кнопку
+        return id(self)
 
 
 class MapButton (Button):
@@ -88,26 +74,49 @@ class Menu (BaseForm):
     def get_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             for element in self.sprites():
-                if element.rect.collidepoint(*event) and isinstance(element, Button):
+                if element.rect.collidepoint(event.pos) and isinstance(element, Button):
                     return element.on_click()
 
 
+class MainMenu (Menu):
+    def __init__(self, *args, **kwargs):
+        super(MainMenu, self).__init__(*args, **kwargs)
+        Button(text("Начать игру", pygame.Color("Red")), 200, 100, 200, 100, pygame.Color("Gray"), self)
+        Button(text("Инструкции", pygame.Color("Red")), 200, 300, 200, 100, pygame.Color("Gray"), self)
+
+
+class LevelMenu(Menu):
+    def __init__(self, *args, **kwargs):
+        super(LevelMenu, self).__init__(*args, **kwargs)
+        size = (89, 89)
+        counter = 0
+        for y in range(50, 468, 139):
+            for x in range(50, 468, 139):
+                counter += 1
+                Button(text("Map " + str(counter),
+                            pygame.Color("Red")), y, x, *size, pygame.Color("Gray"), self)
+
+
 if __name__ == "__main__":  # для тестирования классов интерфейса
-    mb = MessageBox(text("MessageBox 1", pygame.Color("Red")), 100, 100, 200, 200)
-    mb.append(Button(text("Button1", pygame.Color("Red")), 0, 50, 200, 50, mb))
-    print(mb.buttons)
+
+    # menu = MainMenu(*screen_rect, pygame.Color("Gray"))
+
+    # menu = LevelMenu(*screen_rect, pygame.Color("Gray"))
+
+    menu = MessageBox(text("MessageBox 1", pygame.Color("Red")), 100, 100, 200, 50)
+    menu.add(Button(text("Дизайн", pygame.Color("Red")), 100, 100, *size, pygame.Color("Gray")))
+
     running = True
     while running:
         for ev in pygame.event.get():
-            mb.update(ev)
+
+            print(menu.get_event(ev))
+
             if ev.type == pygame.QUIT:
                 running = False
-            if ev.type == pygame.MOUSEBUTTONDOWN:
-                for button in mb.buttons:
-                    button.on_click(ev)
 
-        screen.fill((100, 100, 100))
-        mb.draw(screen)
+        screen.fill((0, 0, 0))
+        menu.draw(screen)
         pygame.display.flip()
 
     pygame.quit()
