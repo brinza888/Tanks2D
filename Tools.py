e3eb9de5c47d5edf260f15a1cd39e79cf55f2ef1
Stasivs <stasivs@bk.ru>
2019-02-17 17:15:00 +0300

    Add presentation; Video;Change tanks image from 30x30 to 28x28

--- Tools.py
+++ Tools.py
@@ -11,7 +11,7 @@ def load_image(name, color_key=None):
         image = pygame.image.load(fullname)
         image = image.convert_alpha()
         if color_key is not None:
-            if color_key is -1:
+            if color_key == -1:
                 color_key = image.get_at((0, 0))
             image.set_colorkey(color_key)
         return image
